4447997578c919d0e06fe3d456026da86f99c2bd
MaxOnn10 <maks.melenevskiy@gmail.com>
2022-07-05 23:11:03 +0300

    burger-menu

--- app/js/main.js
+++ app/js/main.js
@@ -6,20 +6,55 @@ let f5 = document.querySelector(".products__list-item--5");
 let f6 = document.querySelector(".products__list-item--6");
 let img = document.querySelector(".products__decor-img");
 let itemsContainer = document.querySelector(".products__list");
+let burger = document.querySelector(".burger");
+let menuItem = document.querySelector(".header__menu-item");
+let body = document.body;
 function imgSwap() {
 
 }
 // f1.addEventListener('click', function(){
 //     img.src = '../images/products-decor.jpg';
 // });
-itemsContainer.addEventListener('click', function(e){
-    const items = document.querySelectorAll('.products__item-text')
-	const target = e.target
+itemsContainer.addEventListener('click', function (e) {
+  const items = document.querySelectorAll('.products__item-text')
+  const target = e.target
   Array.from(items).forEach(item => {
-  	item.classList.remove('active')
+    item.classList.remove('active')
   })
   target.classList.add('active');
-})
+});
+
+burger.addEventListener('click', function () {
+  burger.classList.toggle("burger--active");
+  document.querySelector(".header__menu").classList.toggle("active"),
+  document.querySelector(".header__menu-list").classList.toggle("active"),
+  document.querySelector(".header__menu-item").classList.toggle("active"),
+  body.classList.toggle("disable-scroll");
+  
+});
+
+menuItem.addEventListener("click", function(){
+  document.querySelector(".header__menu").classList.remove("active"),
+  document.querySelector(".header__menu-list").classList.remove("active"),
+  document.querySelector(".header__menu-item").classList.remove("active"),
+  body.classList.remove("disable-scroll");
+});
+
+
+
+// $(document).ready(function () {
+//   $(".burger").click(function (e) {
+//     $(".burger").toggleClass("burger--active"),
+//       $(".header__menu,.header__menu-list,.header__menu-item,.home__language").toggleClass("active"),
+//       $("body").toggleClass("disable-scroll")
+//   }),
+//     $(".menu__list-item").click(function (e) {
+//       $(".header__menu,.header__menu-list,.header__menu-item,.home__language").removeClass("active"),
+//         $(".burger--active").removeClass("burger--active"),
+//         $("body").removeClass("disable-scroll")
+//     })
+// });
+
 
 // f2.addEventListener('click', function () {
 //     img.src = '../images/main-pictures2.png';
