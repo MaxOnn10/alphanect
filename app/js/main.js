b7ddc6d02755f77947375683eb5a91626b26993f
MaxOnn10 <maks.melenevskiy@gmail.com>
2022-07-06 19:45:10 +0300

    js fixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,61 +1,41 @@
-let f1 = document.querySelector(".products__list-item--1");
-let f2 = document.querySelector(".products__list-item--2");
-let f3 = document.querySelector(".products__list-item--3");
-let f4 = document.querySelector(".products__list-item--4");
-let f5 = document.querySelector(".products__list-item--5");
-let f6 = document.querySelector(".products__list-item--6");
-let img = document.querySelector(".products__decor-img");
-let itemsContainer = document.querySelector(".products__list");
+let itemsContainer = document.querySelector(".products__list-item");
 let burger = document.querySelector(".burger");
-let menuItem = document.querySelector(".header__menu-item");
 let body = document.body;
-function imgSwap() {
+let parent = document.querySelector('.products__list');
+let menuListItem = parent.querySelectorAll('.products__list-item');
+let icon = document.querySelector('.products__item-icon');
 
-}
-// f1.addEventListener('click', function(){
-//     img.src = '../images/products-decor.jpg';
-// });
-itemsContainer.addEventListener('click', function (e) {
-  const items = document.querySelectorAll('.products__item-text')
-  const target = e.target
-  Array.from(items).forEach(item => {
-    item.classList.remove('active')
-  })
-  target.classList.add('active');
+parent.addEventListener('click', (event) => {
+  let target = event.target;
+
+  if (target.classList.contains('products__list-item')) {
+    for (let i = 0; i < menuListItem.length; i++) {
+      menuListItem[i].classList.remove('active');
+      icon.classList.remove('active');
+      //изменение картинки по data-src
+      const srcAttr = target.getAttribute('data-src');
+      document.querySelector('.products__decor-img').src = `${srcAttr}`;
+      document.querySelector('.products__decor-img--mob').src = `${srcAttr}`;
+    }
+    target.classList.add('active');
+  }
+
 });
 
+//burger-menu
 burger.addEventListener('click', function () {
   burger.classList.toggle("burger--active");
   document.querySelector(".header__menu").classList.toggle("active"),
   document.querySelector(".header__menu-list").classList.toggle("active"),
   document.querySelector(".header__menu-item").classList.toggle("active"),
   body.classList.toggle("disable-scroll");
-  
+
 });
 
-menuItem.addEventListener("click", function(){
+document.querySelector('.header__menu-list').addEventListener("click", function () {
   document.querySelector(".header__menu").classList.remove("active"),
   document.querySelector(".header__menu-list").classList.remove("active"),
   document.querySelector(".header__menu-item").classList.remove("active"),
   body.classList.remove("disable-scroll");
 });
 
-
-
-// $(document).ready(function () {
-//   $(".burger").click(function (e) {
-//     $(".burger").toggleClass("burger--active"),
-//       $(".header__menu,.header__menu-list,.header__menu-item,.home__language").toggleClass("active"),
-//       $("body").toggleClass("disable-scroll")
-//   }),
-//     $(".menu__list-item").click(function (e) {
-//       $(".header__menu,.header__menu-list,.header__menu-item,.home__language").removeClass("active"),
-//         $(".burger--active").removeClass("burger--active"),
-//         $("body").removeClass("disable-scroll")
-//     })
-// });
-
-
-// f2.addEventListener('click', function () {
-//     img.src = '../images/main-pictures2.png';
-// });
